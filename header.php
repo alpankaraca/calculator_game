055112a4921c18ef9c9dd3504dbc43297d2ca541
alpankaraca <gs1905>
2014-12-05 01:38:21 +0200

    fg

--- header.php
+++ header.php
@@ -6,6 +6,10 @@
  * Time: 09:40
  */
 
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+
 $link = mysqli_connect('localhost', 'root', '8jy90v');
 if (!$link) {
     die('Could not connect: ' . mysql_error());
