eb38bd6ef74a3c01901cea663a66544328073c18
alpankaraca <gs1905>
2014-12-05 14:58:18 +0200

    hadi

--- header.php
+++ header.php
@@ -10,6 +10,9 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
 
+
+session_start();
+
 $link = mysqli_connect('localhost', 'root', '8jy90v', 'calculator');
 if (!$link) {
     die('Could not connect: ' . mysql_error());
