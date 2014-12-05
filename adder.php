13aaebc5757aee0361ad529ec9f982b95cb54046
alpankaraca <gs1905>
2014-12-05 14:21:20 +0200

    hadi

--- adder.php
+++ adder.php
@@ -22,28 +22,17 @@ if ($result == $given){
 
     $sql = "UPDATE user SET score= score+1 WHERE username='$username'";
     if (mysqli_query($link, $sql)) {
-        $score_table = "SELECT * from user WHERE username='$username'";
-        $score = mysqli_query($link, $score_table);
 
-        while($row = mysqli_fetch_assoc($score)) {
-            echo "Name: " . $row["username"]. " - Score: " . $row["score"] . "<br>";
-        }
     } else {
         echo "Error updating record: " . mysqli_error($link);
     }
 
 
-	echo "Congragulations you did a GREAT job! " . $result;
-	echo "<br />Your score is: " . $score . "<br />";
-	echo '<img src="http://treasure.diylol.com/uploads/post/image/342045/resized_success-kid-meme-generator-congratulations-you-big-girl-170c10.jpg" style="width: 200px;"/>';
+	//echo "Congragulations you did a GREAT job! " . $result;
+	//echo "<br />Your score is: " . $score . "<br />";
+	//echo '<img src="http://treasure.diylol.com/uploads/post/image/342045/resized_success-kid-meme-generator-congratulations-you-big-girl-170c10.jpg" style="width: 200px;"/>';
 }else{
 	echo "<br />Your answer, " . $given . " is not correct! Go back and try again";
-	if ($_SESSION['score'] <= 0) {
-                echo "Score can't be less than 0";
-        }else{
-                $_SESSION['score'] -= 1;
-		echo "Your score is: " . $_SESSION['score'];
-        }
 	echo '<br /><img src="http://www.guitarmasterclass.net/guitar_forum/uploads/monthly_04_2013/post-17069-1366803213.jpg" />' ;
 }
 ?>
---
+++ correct.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alpankaraca
+ * Date: 05/12/14
+ * Time: 14:17
+ */
+
+
+$score_table = "SELECT * from user WHERE username='$username'";
+$score = mysqli_query($link, $score_table);
+
+while($row = mysqli_fetch_assoc($score)) {
+    echo "Name: " . $row["username"]. " - Score: " . $row["score"] . "<br>";
+}
+
+
+?>
+
+
+<img src="http://treasure.diylol.com/uploads/post/image/342045/resized_success-kid-meme-generator-congratulations-you-big-girl-170c10.jpg" style="width: 200px;"/>
