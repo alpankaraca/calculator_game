55f068004cc841b2a7b8cf6eeb3790925e7ca137
alpankaraca <gs1905>
2014-12-05 14:58:51 +0200

    hadi

--- correct.php
+++ correct.php
@@ -12,11 +12,11 @@ $score_table = "SELECT * from user WHERE username='$username'";
 $score = mysqli_query($link, $score_table);
 
 while($row = mysqli_fetch_assoc($score)) {
-    echo "Name: " . $row["username"]. " - Score: " . $row["score"] . "<br>";
+    echo "Name: " . $row["username"]. " <br> Score: " . $row["score"] . "<br>";
 }
 
 
 ?>
 
 
-<img src="http://treasure.diylol.com/uploads/post/image/342045/resized_success-kid-meme-generator-congratulations-you-big-girl-170c10.jpg" style="width: 200px;"/>
+<img src="http://treasure.diylol.com/uploads/post/image/342045/resized_success-kid-meme-generator-congratulations-you-big-girl-170c10.jpg" style="width: 400px;"/>
